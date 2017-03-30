cf99b5ba7be0de736e538d1c5fd1865bfdc2b08c
Delphine Lepers <delphine.lepers@ext.ec.europa.eu>
2017-03-30 17:09:24 +0200

    NEPT-508 - Upgrade theme to version ec_resp_20

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Europa Commission theme
 
 This package is an archive of the older version of ec_resp.
-In order to use it, you should rename ec_resp folder with ec_resp_16
+In order to use it, you should rename ec_resp folder with ec_resp_20
--- theme-settings.php
+++ theme-settings.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * Default theme settings.
@@ -7,7 +8,7 @@
 /**
  * Add toogle option to theme settings to enable/disable dropdown menu.
  */
-function ec_resp_16_form_system_theme_settings_alter(&$form, &$form_state) {
+function ec_resp_20_form_system_theme_settings_alter(&$form, &$form_state) {
   $form['dropdown_fieldset'] = array(
     '#type' => 'fieldset',
     '#title' => t('Dropdown menu settings'),
